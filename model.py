a9d2c7767f4540b43fd8ee2ad81b15cd3af56bff
adjkant <adjkant@gmail.com>
2016-01-23 15:11:57 -0500

    model

---
+++ model.py
@@ -0,0 +1,22 @@
+class User:
+    userNum = 1000
+    def __init__(self, email, phone, password):
+        self.email = email
+        self.phone = phone
+        self.password = password
+        self.houseCode = None
+
+    def getEmail(self):
+        return self.email
+
+    def getPhone(self):
+        return self.phone
+
+    def getPassword(self):
+        return self.password
+
+    def getCode(self):
+        return self.houseCode
+
+    def setCode(self, newCode):
+        self.houseCode = newCode
