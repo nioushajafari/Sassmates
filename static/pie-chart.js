5f82cce3890783ce2e7c32fcc4a049bfea6ef0ab
jaclyn@github.ccs.neu.edu <iulianetti.j@husky.neu.edu>
2016-01-23 16:39:13 -0500

    update data

--- static/pie-chart.js
+++ static/pie-chart.js
@@ -10,8 +10,8 @@ var arc = d3.svg.arc()
     .innerRadius(0);
 
 var labelArc = d3.svg.arc()
-    .outerRadius(radius - 40)
-    .innerRadius(radius - 40);
+    .outerRadius(radius - 200)
+    .innerRadius(radius - 200);
 
 var pie = d3.layout.pie()
     .sort(null)
@@ -20,28 +20,28 @@ var pie = d3.layout.pie()
 var svg = d3.select("#mychart").append("svg")
     .attr("width", width)
     .attr("height", height)
-  .append("g")
+    .append("g")
     .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
 d3.csv("/static/data.csv", type, function(error, data) {
-  if (error) throw error;
+    if (error) throw error;
 
-  var g = svg.selectAll(".arc")
-      .data(pie(data))
-    .enter().append("g")
-      .attr("class", "arc");
+    var g = svg.selectAll(".arc")
+        .data(pie(data))
+        .enter().append("g")
+        .attr("class", "arc");
 
-  g.append("path")
-      .attr("d", arc)
-      .style("fill", function(d) { return color(d.data.name); });
+    g.append("path")
+        .attr("d", arc)
+        .style("fill", function(d) { return color(d.data.name); });
 
-  g.append("text")
-      .attr("transform", function(d) { return "translate(" + labelArc.centroid(d) + ")"; })
-      .attr("dy", ".35em")
-      .text(function(d) { return d.data.name; });
+    g.append("text")
+        .attr("transform", function(d) { return "translate(" + labelArc.centroid(d) + ")"; })
+        .attr("dy", ".35em")
+        .text(function(d) { return d.data.name; });
 });
 
 function type(d) {
-  d.roommates = +d.roommates;
-  return d;
+    d.roommates = +d.roommates;
+    return d;
 }
