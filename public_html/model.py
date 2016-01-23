6345201bbe72335abadc4128952a17c89e27c3e7
adjkant <adjkant@gmail.com>
2016-01-23 14:47:42 -0500

    model adds

--- public_html/model.py
+++ public_html/model.py
@@ -1,7 +1,22 @@
-class TestClass:
+class User:
     userNum = 1000
-    def __init__(self, email):
-        #constructor
-    def f(self):
-        #method
-        return 0
+    def __init__(self, email, phone, password):
+        self.email = email
+        self.phone = phone
+        self.password = password
+        self.houseCode = None
+git commi
+    def getEmail(self):
+        return self.email
+
+    def getPhone(self):
+        return self.phone
+
+    def getPassword(self):
+        return self.password
+
+    def getCode(self):
+        return self.houseCode
+
+    def setCode(self, newCode):
+        self.houseCode = newCode
